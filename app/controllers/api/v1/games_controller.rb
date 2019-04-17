8cf00e369cb23bbe6c36ad02d465e70a9972963a
Ezra Schwepker <ezraschwepker@gmail.com>
2019-04-16 17:53:40 -0700

    saved games controller changes

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,41 +2,41 @@ class Api::V1::GamesController < ApplicationController
   skip_before_action :authorized, only: [:create]
   before_action :find_game, only: [:edit, :update, :destroy]
 
-  def index
-    @games = Game.all
-    render json: @games
-  end
+  # def index
+  #   @games = Game.all
+  #   render json: @games
+  # end
 
-  def create
-    @game = Game.create(
-                        player1_id: game_params[:player1_id],
-                        player2_id: game_params[:player2_id],
-                        turn: 0,
-                        finished: false,
-                        winner: 0)
-    render json: @game
-  end
+  # def create
+  #   @game = Game.create(
+  #                       player1_id: game_params[:player1_id],
+  #                       player2_id: game_params[:player2_id],
+  #                       turn: 0,
+  #                       finished: false,
+  #                       winner: 0)
+  #   render json: @game
+  # end
 
-  def update
-    @game.update(game_params)
-    if @game.save
-      render json: @game, status: :accepted
-    else
-      render json: 
-        { errors: @game.errors_full_messages },
-        status: :unprocessible_entity
-    end
-  end
+  # def update
+  #   @game.update(game_params)
+  #   if @game.save
+  #     render json: @game, status: :accepted
+  #   else
+  #     render json: 
+  #       { errors: @game.errors_full_messages },
+  #       status: :unprocessible_entity
+  #   end
+  # end
 
-  def destroy
-    if @game.destroy
-      render json: { game_destroyed: true }, status: :accepted
-    else
-      render json: 
-        { errors: @game.errors_full_messages },
-        status: :unprocessible_entity
-    end
-  end
+  # def destroy
+  #   if @game.destroy
+  #     render json: { game_destroyed: true }, status: :accepted
+  #   else
+  #     render json: 
+  #       { errors: @game.errors_full_messages },
+  #       status: :unprocessible_entity
+  #   end
+  # end
 
   def active
     # byebug
@@ -51,18 +51,18 @@ class Api::V1::GamesController < ApplicationController
     end
   end
   
-  def available
-    # byebug
-    @available_games = Game
-                      .all.to_a
-                      .select {|game| !game.finished && game.player2_id.nil?}
+  # def available
+  #   # byebug
+  #   @available_games = Game
+  #                     .all.to_a
+  #                     .select {|game| !game.finished && game.player2_id.nil?}
     
-    unless @available_games.empty?
-      render json: @available_games, each_serializer: GameSerializer
-    else
-      render json: { status: 'no available games', ready: false }
-    end
-  end
+  #   unless @available_games.empty?
+  #     render json: @available_games, each_serializer: GameSerializer
+  #   else
+  #     render json: { status: 'no available games', ready: false }
+  #   end
+  # end
 
   def create
     byebug
