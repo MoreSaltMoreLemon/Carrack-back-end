3e7717534fff1e1704578bd72367ce7ae08d56f2
allen-woods <allen.woods@flatironschool.com>
2019-04-17 12:56:49 -0700

    Building state transfer through the server.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,43 +2,7 @@ require_relative './initial_game.rb'
 
 class Api::V1::GamesController < ApplicationController
   skip_before_action :authorized, only: [:create]
-  before_action :find_game, only: [:edit, :update, :destroy]
-
-  # def index
-  #   @games = Game.all
-  #   render json: @games
-  # end
-
-  # def create
-  #   @game = Game.create(
-  #                       player1_id: game_params[:player1_id],
-  #                       player2_id: game_params[:player2_id],
-  #                       turn: 0,
-  #                       finished: false,
-  #                       winner: 0)
-  #   render json: @game
-  # end
-
-  # def update
-  #   @game.update(game_params)
-  #   if @game.save
-  #     render json: @game, status: :accepted
-  #   else
-  #     render json: 
-  #       { errors: @game.errors_full_messages },
-  #       status: :unprocessible_entity
-  #   end
-  # end
-
-  # def destroy
-  #   if @game.destroy
-  #     render json: { game_destroyed: true }, status: :accepted
-  #   else
-  #     render json: 
-  #       { errors: @game.errors_full_messages },
-  #       status: :unprocessible_entity
-  #   end
-  # end
+  before_action :find_game, only: [:turn?, :import, :export, :win, :exit]
 
   def active
     # byebug
@@ -52,19 +16,6 @@ class Api::V1::GamesController < ApplicationController
       render json: { status: 'no active games', ready: false }
     end
   end
-  
-  # def available
-  #   # byebug
-  #   @available_games = Game
-  #                     .all.to_a
-  #                     .select {|game| !game.finished && game.player2_id.nil?}
-    
-  #   unless @available_games.empty?
-  #     render json: @available_games, each_serializer: GameSerializer
-  #   else
-  #     render json: { status: 'no available games', ready: false }
-  #   end
-  # end
 
   def create
     # byebug
@@ -100,10 +51,12 @@ class Api::V1::GamesController < ApplicationController
   end
 
   def import
+    byebug
     render json: @game
   end
 
   def export
+    # byebug
     @game.update(game_params)
     if @game.save
       render json: @game, status: :accepted
